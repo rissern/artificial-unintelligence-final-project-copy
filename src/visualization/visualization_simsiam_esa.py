7f8f65672208a80fd73be7260138b839ccc17a93
Joshua Cordero <joshcordero2134@gmail.com>
2024-06-12 13:19:15 -0700

    updated simclr visualizations to work with out datamodule and out trained models

--- src/visualization/visualization_simsiam_esa.py
+++ src/visualization/visualization_simsiam_esa.py
@@ -1,309 +1,193 @@
 # Import
-import math
-import os
 from pathlib import Path
 import sys
 
 sys.path.append(".")
 import numpy as np
 import torch
-import torch.nn as nn
-import torchvision
-import torchvision.transforms.functional as functional
-
-from lightly.data import LightlyDataset
-from lightly.loss import NegativeCosineSimilarity
-from lightly.models.modules.heads import SimSiamPredictionHead, SimSiamProjectionHead
-from lightly.transforms import SimCLRTransform, utils
 
 import matplotlib.offsetbox as osb
 import matplotlib.pyplot as plt
 from matplotlib import rcParams as rcp
-from matplotlib.offsetbox import (AnnotationBbox, DrawingArea, OffsetImage,
-                                  TextArea)
+from matplotlib.offsetbox import (AnnotationBbox, OffsetImage)
 from PIL import Image
 
 from sklearn import random_projection
 from src.models.selfsupervised.satellite_module import ESDSelfSupervised
-from scripts.unsupervised.train import train
 
-# Configuration
-num_workers = 0
-batch_size = 128
-seed = 1
-epochs = 50
-input_size = 256
-num_ftrs = 512
-out_dim = proj_hidden_dim = 512
-pred_hidden_dim = 128
+from src.utilities import ESDConfig, SatelliteType
+from src.esd_data.datamodule import ESDDataModule
+from src.visualization import restitch_plot
+from src.preprocessing.subtile import Subtile
 
-torch.manual_seed(seed)
-np.random.seed(seed)
+ROOT = Path.cwd()
 
-path_to_data = "/Users/yeseongmoon/data/processed/Sentinal-2/"
+def create_embeddings(options: ESDConfig, model_path: Path):
 
-# define the augmentations for self-supervised learning
-transform = SimCLRTransform(
-    input_size=input_size,
-    hf_prob=0.5,
-    vf_prob=0.5,
-    rr_prob=0.5,
-    min_scale=0.5,
-    cj_prob=0.2,
-    cj_bright=0.1,
-    cj_contrast=0.1,
-    cj_hue=0.1,
-    cj_sat=0.1,
-)
+    dataModule = ESDDataModule(
+        processed_dir=options.processed_dir,
+        raw_dir=options.raw_dir,
+        batch_size=options.batch_size,
+        seed=options.seed,
+        selected_bands=options.selected_bands,
+        slice_size=options.slice_size,
+    )
 
-# create a lightly dataset for training with augmentations
-dataset_train_simsiam = LightlyDataset(input_dir=path_to_data, transform=transform)
+    # prepare data
+    dataModule.prepare_data()
+    dataModule.setup("fit")
 
-# create a dataloader for training
-dataloader_train_simsiam = torch.utils.data.DataLoader(
-    dataset_train_simsiam,
-    batch_size=batch_size,
-    shuffle=True,
-    drop_last=True,
-    num_workers=num_workers,
-)
+    # load model from checkpoint and set to eval mode
+    esdselfsup = ESDSelfSupervised.load_from_checkpoint(model_path)
+    esdselfsup.eval()
 
-# create a torchvision transformation for embedding the dataset after training
-# here, we resize the images to match the input size during training and apply
-# a normalization of the color channel based on statistics from imagenet
-test_transforms = torchvision.transforms.Compose(
-    [
-        torchvision.transforms.Resize((input_size, input_size)),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(
-            mean=utils.IMAGENET_NORMALIZE["mean"],
-            std=utils.IMAGENET_NORMALIZE["std"],
-        ),
+    # get a list of all processed files
+    all_processed_files = [
+        tile for tile in (Path(options.processed_dir) / "Val" / "subtiles").iterdir() if tile.is_dir()
     ]
-)
 
-# create a lightly dataset for embedding
-dataset_test = LightlyDataset(input_dir=path_to_data, transform=test_transforms)
+    embeddings = []
+    color_images = []
 
-# create a dataloader for embedding
-dataloader_test = torch.utils.data.DataLoader(
-    dataset_test,
-    batch_size=batch_size,
-    shuffle=False,
-    drop_last=False,
-    num_workers=num_workers,
-)
+    # get embeddings for all tiles
+    for tile in all_processed_files:
 
-# Create the SimSiam model
+        subtile = Subtile(
+            satellite_list=[],
+            ground_truth=[],
+            slice_size=options.slice_size,
+            parent_tile_id=tile.name,
+        )
 
+        subtile.restitch(options.processed_dir / "Val", dataModule.satellite_type_list)
 
-class SimSiam(nn.Module):
-    def __init__(self, backbone, num_ftrs, proj_hidden_dim, pred_hidden_dim, out_dim):
-        super().__init__()
-        self.backbone = backbone
-        self.projection_head = SimSiamProjectionHead(num_ftrs, proj_hidden_dim, out_dim)
-        self.prediction_head = SimSiamPredictionHead(out_dim, pred_hidden_dim, out_dim)
+        # get the embeddings
+        for i in range(options.slice_size[0]):
+            for j in range(options.slice_size[1]):
+                X, _ = restitch_plot.retrieve_subtile_file(
+                    i, j, processed_dir=options.processed_dir / "Val", parent_tile_id=tile.name, datamodule=dataModule
+                )
 
-    def forward(self, x):
-        # get representations
-        f = self.backbone(x).flatten(start_dim=1)
-        # get projections
-        z = self.projection_head(f)
-        # get predictions
-        p = self.prediction_head(z)
-        # stop gradient
-        z = z.detach()
-        return z, p
+                # get sentinel-2 image
+                sentinel_2 = subtile.load_subtile(options.processed_dir / "Val", [SatelliteType.S2], i, j)[0]
+                rgb_image = (sentinel_2.sel(band=["04", "03", "02"]).to_numpy()[0].transpose(1, 2, 0) * 255).astype(np.uint8)
 
+                # reshape the data to fit the model
+                X = X.reshape((1, X.shape[-3], X.shape[-2], X.shape[-1]))
 
-# we use a pretrained resnet for this tutorial to speed
-# up training time but you can also train one from scratch
+                # move the image to the accelerator
+                X = X.float().to(options.accelerator)
+                embedding = esdselfsup.model.backbone(X.float())[-1]
+                embedding = esdselfsup.model.adaptive_pool(embedding).flatten(start_dim=1)
+                
+                embedding = embedding.detach()
 
-device = (
-    torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
-)
-model = ESDSelfSupervised("SimClr", 3, 128, 0.06, {})
-model.train()
+                embeddings.append(embedding)
+                color_images.append(rgb_image)
 
-# SimSiam uses a symmetric negative cosine similarity loss and does therefore
-# not require any negative samples. We build a criterion and an optimizer.
+    embeddings = torch.cat(embeddings, dim=0)
+    embeddings = embeddings.cpu().numpy()
 
-# SimSiam uses a symmetric negative cosine similarity loss
-criterion = NegativeCosineSimilarity()
+    projection = random_projection.GaussianRandomProjection(n_components=2)
+    embeddings_2d = projection.fit_transform(embeddings)
 
-# scale the learning rate
-lr = 0.05 * batch_size / 256
-# use SGD with momentum and weight decay
-optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
+    # normalize the embeddings to fit in the [0, 1] square
+    M = np.max(embeddings_2d, axis=0)
+    m = np.min(embeddings_2d, axis=0)
+    embeddings_2d = (embeddings_2d - m) / (M - m)
 
-# Train SimSiam
-# --------------------
-
-model.to(device)
-
-# To embed the images in the dataset we simply iterate over the test dataloader
-# and feed the images to the model backbone. Make sure to disable gradients for
-# this part.
-
-embeddings = []
-filenames = []
-
-# disable gradients for faster calculations
-model.eval()
-with torch.no_grad():
-    for i, (x, _, fnames) in enumerate(dataloader_test):
-        # move the images to the gpu
-        x = x.to(device)
-        # embed the images with the pre-trained backbone
-        y = model.model.backbone(x).flatten(start_dim=1)
-        # store the embeddings and filenames in lists
-        embeddings.append(y)
-        filenames = filenames + list(fnames)
-
-# concatenate the embeddings and convert to numpy
-embeddings = torch.cat(embeddings, dim=0)
-embeddings = embeddings.cpu().numpy()
+    return embeddings, embeddings_2d, color_images
 
 
-# Scatter Plot and Nearest Neighbors
-# ----------------------------------
-# Now that we have the embeddings, we can visualize the data with a scatter plot.
-# Further down, we also check out the nearest neighbors of a few example images.
-#
-# As a first step, we make a few additional imports.
+def plot_embeddings(embeddings, color_images):
 
-
-# Then, we transform the embeddings using UMAP and rescale them to fit in the
-# [0, 1] square.
-
-# for the scatter plot we want to transform the images to a two-dimensional
-# vector space using a random Gaussian projection
-projection = random_projection.GaussianRandomProjection(n_components=2)
-embeddings_2d = projection.fit_transform(embeddings)
-
-# normalize the embeddings to fit in the [0, 1] square
-M = np.max(embeddings_2d, axis=0)
-m = np.min(embeddings_2d, axis=0)
-embeddings_2d = (embeddings_2d - m) / (M - m)
-
-def get_image_as_np_array(filename: str):
-    """Loads the image with filename and returns it as a numpy array."""
-    img = Image.open(filename)
-    if img.mode != "RGB":
-        img = img.convert("RGB")
-    return np.asarray(img)
-
-def get_scatter_plot_with_thumbnails():
-    """Creates a scatter plot with image overlays."""
-    # initialize empty figure and add subplot
     fig = plt.figure()
-    fig.suptitle("Scatter Plot of the Sentinel-2 Dataset")
+    fig.suptitle("Scatter Plot of the Sentinel-2 VIIRS Dataset")
     ax = fig.add_subplot(1, 1, 1)
     # shuffle images and find out which images to show
     shown_images_idx = []
     shown_images = np.array([[1.0, 1.0]])
-    iterator = [i for i in range(embeddings_2d.shape[0])]
+    iterator = [i for i in range(embeddings.shape[0])]
     np.random.shuffle(iterator)
     for i in iterator:
         # only show image if it is sufficiently far away from the others
-        dist = np.sum((embeddings_2d[i] - shown_images) ** 2, 1)
+        dist = np.sum((embeddings[i] - shown_images) ** 2, 1)
         if np.min(dist) < 2e-3:
             continue
-        shown_images = np.r_[shown_images, [embeddings_2d[i]]]
+        shown_images = np.r_[shown_images, [embeddings[i]]]
         shown_images_idx.append(i)
 
     # plot image overlays
     for idx in shown_images_idx:
-        # thumbnail_size = int(rcp["figure.figsize"][0] * 2.0)
-        # path = os.path.join(path_to_data, filenames[idx])
-        # img = get_image_as_np_array(path)
-        # # img = Image.open(path)
-        # img = functional.resize(Image.fromarray(img), thumbnail_size)
-        # img = np.array(img)
-        # img_box = osb.AnnotationBbox(
-        #     osb.OffsetImage(img, cmap=plt.cm.gray_r),
-        #     embeddings_2d[idx],
-        #     pad=0.2,
-        # )
-        # ax.add_artist(img_box)
-    
-        thumbnail_size = int(plt.rcParams["figure.figsize"][0] * 2.0)
-        path = os.path.join(path_to_data, filenames[idx])
-        img = get_image_as_np_array(path)
+        thumbnail_size = int(rcp["figure.figsize"][0] * 2.0)
+        img = color_images[idx]
         img = Image.fromarray(img).resize((thumbnail_size, thumbnail_size))
-        img_box = AnnotationBbox(OffsetImage(img), embeddings_2d[idx], pad=0.2)
+        img = np.array(img)
+        img_box = AnnotationBbox(
+            OffsetImage(img, cmap=plt.cm.gray_r), 
+            embeddings[idx], 
+            pad=0.2
+            )
         ax.add_artist(img_box)
 
-    ax.set_aspect('equal')
+    # set aspect ratio
+    ratio = 1.0 / ax.get_data_ratio()
+    ax.set_aspect(ratio, adjustable="box")
+
     plt.show()
 
-    # set aspect ratio
-    # ratio = 1.0 / ax.get_data_ratio()
-    # ax.set_aspect(ratio, adjustable="box")
 
 
-
-# get a scatter plot with thumbnail overlays
-get_scatter_plot_with_thumbnails()
-
-# Next, we plot example images and their nearest neighbors (calculated from the
-# embeddings generated above). This is a very simple approach to find more images
-# of a certain type where a few examples are already available. For example,
-# when a subset of the data is already labelled and one class of images is clearly
-# underrepresented, one can easily query more images of this class from the
-# unlabelled dataset.
-
-# Let's get to work! The plots are shown below.
-
-images = list()
-for root, dirnames, filenames in os.walk(path_to_data):
-    for file in filenames:
-        images.append(file)
-
-
-def get_image_as_np_array_with_frame(filename: str, w: int = 5):
+def frame_np_image(image_np: np.ndarray, w: int = 5):
     """Returns an image as a numpy array with a black frame of width w."""
-    img = get_image_as_np_array(filename)
-    ny, nx, _ = img.shape
+    ny, nx, _ = image_np.shape
     # create an empty image with padding for the frame
     framed_img = np.zeros((w + ny + w, w + nx + w, 3), dtype=np.uint8)
     #framed_img = framed_img.astype(np.uint8)
     # put the original image in the middle of the new one
-    framed_img[w:-w, w:-w] = img
+    framed_img[w:-w, w:-w] = image_np
     return framed_img
 
 
-def plot_nearest_neighbors_3x3(example_image: str, i: int):
+def plot_nearest_neighbors_3x3(example_image_idx: int, i: int, embeddings, images_rgb):
     """Plots the example image and its eight nearest neighbors."""
     n_subplots = 9
     # initialize empty figure
     fig = plt.figure()
     fig.suptitle(f"Nearest Neighbor Plot {i + 1}")
     #
-    example_idx = filenames.index(example_image)
     # get distances to the cluster center
     # distances = embeddings - embeddings[example_idx]
     # distances = np.power(distances, 2).sum(-1).squeeze()
     #
-    distances = np.linalg.norm(embeddings - embeddings[example_idx], axis=1)
+    distances = np.linalg.norm(embeddings - embeddings[example_image_idx], axis=1)
     # sort indices by distance to the center
     nearest_neighbors = np.argsort(distances)[:n_subplots]
     # show images
     for plot_offset, plot_idx in enumerate(nearest_neighbors):
         ax = fig.add_subplot(3, 3, plot_offset + 1)
         # get the corresponding filename
-        fname = os.path.join(path_to_data, filenames[plot_idx])
+
         if plot_offset == 0:
             ax.set_title(f"Images")
-            plt.imshow(get_image_as_np_array_with_frame(fname))
+            plt.imshow(frame_np_image(images_rgb[plot_idx]))
         else:
-            plt.imshow(get_image_as_np_array(fname))
+            plt.imshow(images_rgb[plot_idx])
         # let's disable the axis
         plt.axis("off")
     plt.show()
 
-# show example images for each cluster
-for i, image in enumerate(images):
-    if i > 10:
-        break
-    plot_nearest_neighbors_3x3(image, i)
+
+
+if __name__ == "__main__":
+
+    options = ESDConfig()
+
+    model_path = ROOT / "models" / ESDConfig.model_type / "epoch=29-val_loss=1.58-eval_accuracy=0.59.ckpt"
+
+    emb, emb_2d, rgb_images = create_embeddings(options, model_path)
+
+    plot_embeddings(emb_2d, rgb_images)
+
+    for i in range(5):
+        plot_nearest_neighbors_3x3(i, i, emb_2d, rgb_images)
